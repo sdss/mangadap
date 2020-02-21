09bc6ce3e76a88753f6a18903d4bc984738b1d94
Kyle Westfall <kbwestfall@gmail.com>
2020-02-21 11:39:48 -0800

    test scripts

--- mangadap/scripts/write_dap_par.py
+++ mangadap/scripts/write_dap_par.py
@@ -1,33 +1,25 @@
-#!/usr/bin/env python3
-
-#-----------------------------------------------------------------------
 import os
 import time
+import argparse
 
 from mangadap.survey.drpcomplete import DRPComplete
-#-----------------------------------------------------------------------
 
-def write_dap_par():
+def parse_args(options=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('drpcomplete', type=str, help='DRP complete fits file')
+    parser.add_argument('plate', type=int, help='Plate number')
+    parser.add_argument('ifudesign', type=int, help='IFU design number')
+    parser.add_argument('mode', type=str, help='DRP 3D file mode: CUBE or RSS')
+    parser.add_argument('ofile', type=str, help='Output file name')
+    return parser.parse_args() if options is None else parser.parse_args(options)
 
-    if len(sys.argv) != 6:
-        print('Usage: write_par.py <drpcomplete file> <plate> <ifudesign> <(CUBE or RSS)> <output file>')
-        raise Exception('Incorrect number of arguments!')
-
-    root_dir = os.path.dirname(sys.argv[1])
+def main(args):
+    t = time.perf_counter()
+    root_dir = os.path.dirname(args.drpcomplete)
     if len(root_dir) == 0:
         root_dir = '.'
-    drpc_file = sys.argv[1]
-    drpver = drpc_file[drpc_file.find('_v')+1 : drpc_file.find('.fits')]
-
+    drpver = args.drpcomplete[args.drpcomplete.find('_v')+1 : args.drpcomplete.find('.fits')]
     drpc = DRPComplete(drpver=drpver, directory_path=root_dir, readonly=True)
-
-    drpc.write_par(ofile=sys.argv[5], mode=sys.argv[4], plate=int(sys.argv[2]),
-                   ifudesign=int(sys.argv[3]))
-
-
-if __name__ == '__main__':
-    t = time.perf_counter()
-    write_dap_par()
+    drpc.write_par(ofile=args.ofile, mode=args.mode, plate=args.plate, ifudesign=args.ifudesign)
     print('Elapsed time: {0} seconds'.format(time.perf_counter() - t))
-
 
--- mangadap/tests/test_scripts.py
+++ mangadap/tests/test_scripts.py
@@ -13,17 +13,21 @@ import warnings
 warnings.simplefilter("ignore", UserWarning)
 warnings.simplefilter("ignore", RuntimeWarning)
 
-@requires_remote
-def test_calculate_covariance():
-
+def script_help_okay(executable):
     # Basic help call
     logfile = 'test.log'
     if os.path.isfile(logfile):
         os.remove(logfile)
     with open(logfile, 'w') as f:
-        retval = subprocess.call(['dap_calculate_covariance', '-h'], stdout=f, stderr=f)
+        retval = subprocess.call([executable, '-h'], stdout=f, stderr=f)
     os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    return retval == 0
+
+
+@requires_remote
+def test_calculate_covariance():
+
+    assert script_help_okay('dap_calculate_covariance'), 'Basic help call failed'
 
     ofile = 'test.fits'
     if os.path.isfile(ofile):
@@ -50,117 +54,60 @@ def test_calculate_covariance():
     # Clean-up
     os.remove(ofile)
 
+
 # TODO: Add some remote files to test this?
 def test_construct_dapall():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['construct_dapall', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('construct_dapall'), 'Basic help call failed'
+
 
 # TODO: Include this in the construct_dapall test and test the
 # constructed file?
 def test_dapall_qa():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['dapall_qa', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('dapall_qa'), 'Basic help call failed'
+
 
 # TODO: Include a DRPComplete file in remote?
 def test_find_repeat_observations():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['dap_find_repeat_observations', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('dap_find_repeat_observations'), 'Basic help call failed'
+
 
 # TODO: Add some remote files?
 def test_fit_residuals():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['dap_fit_residuals', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('dap_fit_residuals'), 'Basic help call failed'
+
 
 # TODO: Add some remote files?
 def test_inspector():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['manga_dap_inspector', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('manga_dap_inspector'), 'Basic help call failed'
+
 
 # TODO: Sort out an execution mode that can run in a few minutes.
 def test_manga_dap():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['manga_dap', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('manga_dap'), 'Basic help call failed'
+
 
 # TODO: Add some remote data?
 def test_plate_fit_qa():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['dap_plate_fit_qa', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('dap_plate_fit_qa'), 'Basic help call failed'
+
 
 # TODO: Add some remote data?
 def test_ppxffit_qa():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['dap_ppxffit_qa', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('dap_ppxffit_qa'), 'Basic help call failed'
+
 
 # TODO: Add some remote data?
 def test_spotcheck():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['spotcheck_dap_maps', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('spotcheck_dap_maps'), 'Basic help call failed'
+
 
 # TODO: Add some remote data?
 def test_template_flux_norm():
-    # Basic help call
-    logfile = 'test.log'
-    if os.path.isfile(logfile):
-        os.remove(logfile)
-    with open(logfile, 'w') as f:
-        retval = subprocess.call(['dap_template_flux_norm', '-h'], stdout=f, stderr=f)
-    os.remove(logfile)
-    assert retval == 0, 'Basic help call failed'
+    assert script_help_okay('dap_template_flux_norm'), 'Basic help call failed'
 
-if __name__ == '__main__':
-    test_template_flux_norm()
+
+# TODO: Add a remote DRPComplete file!
+def test_write_dap_par():
+    assert script_help_okay('write_dap_par'), 'Basic help call failed'
+
 
