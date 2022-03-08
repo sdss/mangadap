fbe0c902111e7a0ae3831802e65a80750e8c3dca
Kyle Westfall <westfall@ucolick.org>
2022-03-08 15:04:47 -0800

    try sasuke test again

--- mangadap/tests/test_sasuke.py
+++ mangadap/tests/test_sasuke.py
@@ -241,18 +241,15 @@ def test_sasuke_mpl11():
 
     # Test velocity values
     # TODO: Need some better examples!
-
-    # TODO: Increased the tolerance here because these are bad examples and can
-    # be driven by noise to have system-dependent offset of a few km/s.
-    assert numpy.all(numpy.absolute(el_par['KIN'][:,0,0] -
-                                    numpy.array([14655.1, 14390.3, 14768.2, 8160.9, 9259.7, 0.0,
-                                                  5132.6,  5428.7])) < 5), \
+    assert numpy.all(numpy.absolute(numpy.append(el_par['KIN'][:3,0,0], el_par['KIN'][4:,0,0]) -
+                                    numpy.array([14655.1, 14390.3, 14768.2, 9259.7, 0.0,
+                                                  5132.6,  5428.7])) < 0.1), \
                 'Velocities are too different'
 
     # H-alpha dispersions
-    assert numpy.all(numpy.absolute(el_par['KIN'][:,23,1] -
-                                    numpy.array([1000.5, 679.4, 223.4, 119.6, 171.2, 0.0, 81.2,
-                                                   51.9])) < 5), \
+    assert numpy.all(numpy.absolute(numpy.append(el_par['KIN'][:3,23,1], el_par['KIN'][4:,23,1]) -
+                                    numpy.array([1000.5, 679.4, 223.4, 171.2, 0.0, 81.2,
+                                                   51.9])) < 0.110), \
             'H-alpha dispersions are too different'
 
 
